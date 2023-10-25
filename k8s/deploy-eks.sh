862a02100fce98448c7660257f578c79b038cf7f
Mateus Silva <mateussilva@ntopus.com.br>
2023-10-25 16:55:24 -0300

    infra k8s

---
+++ k8s/deploy-eks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kubectl apply -f soatdb-configmap.yml
+
+kubectl apply -f soatdb.yml
+
+kubectl apply -f soatdb-svc.yml
+
+kubectl apply -f soatapp-configmap.yml
+
+kubectl apply -f soatapp.yml
+
+kubectl apply -f soatapp-svc.yml
